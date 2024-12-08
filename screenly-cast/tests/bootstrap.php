e4d21a38bce1664db39808a3b5e7bd99648f07da
Viktor Petersson <self@vpetersson.com>
2024-12-08 19:29:25 +0000

    Attempts to run tests

--- screenly-cast/tests/bootstrap.php
+++ screenly-cast/tests/bootstrap.php
@@ -1,19 +1,11 @@
 <?php
 /**
  * PHPUnit bootstrap file
- *
- * @package Screenly_Cast
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
-
 if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
-	exit( 1 );
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 // Give access to tests_add_filter() function.
