a1fd2a6e94e2759f0d32dce97b1b01c02f52ad8e
Viktor Petersson <self@vpetersson.com>
2024-12-26 20:10:42 +0000

    Runs linter

--- screenly-cast/theme/screenly-cast/assets/js/scripts.js
+++ screenly-cast/theme/screenly-cast/assets/js/scripts.js
@@ -1,41 +1,47 @@
-(function()
-{
-    "use strict";
+(function () {
+	'use strict';
 
-    var article = document.querySelector('article');
-    if (article) {
-        var content = article.querySelector('.content');
-        if (content) {
-            var childs = content.children;
+	const article = document.querySelector('article');
+	if (article) {
+		const content = article.querySelector('.content');
+		if (content) {
+			const childs = content.children;
 
-            /*
-             * LET'S START REMOVING CHILD BLOCKS THAT ARE OUT OF VIEW
-             */
-            var i = childs.length;
-            for (i; i>0; i--) {
-                var child = childs[i];
-                if (typeof child === 'object') {
-                    var childRect = child.getClientRects()[0];
+			/*
+			 * LET'S START REMOVING CHILD BLOCKS THAT ARE OUT OF VIEW
+			 */
+			let i = childs.length;
+			for (i; i > 0; i--) {
+				const child = childs[i];
+				if (typeof child === 'object') {
+					const childRect = child.getClientRects()[0];
 
-                    if (childRect.top > article.clientHeight) {
-                        child.remove(child);
-                    } else if (childRect.top + child.offsetHeight > article.clientHeight) {
-                        /*
-                         * NOW WE NEED TO WORK WORD BY WORD UNTIL NO SCROLL IS NEEDED
-                         */
-                        var words = child.innerText.split(' ');
-                        var tlength = words.length;
-                        var a = tlength;
-                        for (a; a>0; a--) {
-                            child.innerText = words.slice(0, a).join(' ') + '[...]';
+					if (childRect.top > article.clientHeight) {
+						child.remove(child);
+					} else if (
+						childRect.top + child.offsetHeight >
+						article.clientHeight
+					) {
+						/*
+						 * NOW WE NEED TO WORK WORD BY WORD UNTIL NO SCROLL IS NEEDED
+						 */
+						const words = child.innerText.split(' ');
+						const tlength = words.length;
+						let a = tlength;
+						for (a; a > 0; a--) {
+							child.innerText =
+								words.slice(0, a).join(' ') + '[...]';
 
-                            if (childRect.top + child.offsetHeight < article.clientHeight) {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+							if (
+								childRect.top + child.offsetHeight <
+								article.clientHeight
+							) {
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 })();
