b071aa2d2fe7623db0e92380a4dddbcaad31ab30
nicomiguelino <nicomiguelino2014@gmail.com>
2025-01-21 10:43:59 -0800

    fix: broken unit testing CI pipeline

--- tests/docker-entrypoint.sh
+++ tests/docker-entrypoint.sh
@@ -17,14 +17,23 @@ if [ ! -d "$WP_TESTS_DIR" ]; then
     mkdir -p "$WP_TESTS_DIR" "$WP_CORE_DIR"
 
     # Download WordPress
-    curl -o /tmp/wordpress.tar.gz "https://wordpress.org/wordpress-${WP_VERSION}.tar.gz"
-    tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C "$WP_CORE_DIR"
-    rm /tmp/wordpress.tar.gz
+    if [[ $WP_VERSION == 'latest' ]]; then
+        curl -O https://wordpress.org/latest.tar.gz
+        tar --strip-components=1 -zxmf latest.tar.gz -C "$WP_CORE_DIR"
+        rm latest.tar.gz
+    else
+        curl -O https://wordpress.org/wordpress-$WP_VERSION.tar.gz
+        tar --strip-components=1 -zxmf wordpress-$WP_VERSION.tar.gz -C "$WP_CORE_DIR"
+        rm wordpress-$WP_VERSION.tar.gz
+    fi
 
-    # Download test suite
-    svn co --quiet https://develop.svn.wordpress.org/trunk/tests/phpunit/includes/ "$WP_TESTS_DIR/includes"
-    svn co --quiet https://develop.svn.wordpress.org/trunk/tests/phpunit/data/ "$WP_TESTS_DIR/data"
-    curl -o "$WP_TESTS_DIR/wp-tests-config.php" https://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php
+    # Install WordPress test suite
+    svn co --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/tests/phpunit/includes/ "$WP_TESTS_DIR/includes"
+    svn co --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/tests/phpunit/data/ "$WP_TESTS_DIR/data"
+
+    # Download wp-tests-config-sample.php from the appropriate version
+    curl -o "$WP_TESTS_DIR/wp-tests-config.php" \
+        https://develop.svn.wordpress.org/tags/$WP_VERSION/wp-tests-config-sample.php
 
     # Configure test suite
     sed -i "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR/wp-tests-config.php"
@@ -32,6 +41,14 @@ if [ ! -d "$WP_TESTS_DIR" ]; then
     sed -i "s/yourusernamehere/root/" "$WP_TESTS_DIR/wp-tests-config.php"
     sed -i "s/yourpasswordhere/wordpress/" "$WP_TESTS_DIR/wp-tests-config.php"
     sed -i "s|localhost|db-test|" "$WP_TESTS_DIR/wp-tests-config.php"
+
+    # Create wp-content directory
+    mkdir -p "$WP_CORE_DIR/wp-content/plugins"
+fi
+
+# Install dependencies if they're not already installed
+if [ ! -d "vendor" ]; then
+    composer install --no-interaction
 fi
 
 echo "Running command: $@"
