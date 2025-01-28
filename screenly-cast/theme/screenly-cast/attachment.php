7cad23f72724ff43accd4fd150e99c54513405f2
nicomiguelino <nicomiguelino2014@gmail.com>
2025-01-28 15:41:53 -0800

    chore: update code comments

--- screenly-cast/theme/screenly-cast/attachment.php
+++ screenly-cast/theme/screenly-cast/attachment.php
@@ -1,16 +1,10 @@
 <?php
-/**
- * Attachment
+ /**
+ * The template for displaying attachment pages.
  *
- * PHP version 5
- *
- * @category PHP
- * @package  ScreenlyCast
- * @author   Peter Monte <pmonte@screenly.io>
- * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPLv2
- * @link     https://github.com/Screenly/Screenly-Cast-for-WordPress
- * @since    0.0.1
+ * @package ScreenlyCast
  */
+
 defined('ABSPATH') or die("No script kiddies please!");
 
 /**
--- screenly-cast/theme/screenly-cast/footer.php
+++ screenly-cast/theme/screenly-cast/footer.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Footer
+ /**
+ * The template for displaying the footer.
  *
- * PHP version 5
- *
- * @category PHP
- * @package  ScreenlyCast
- * @author   Peter Monte <pmonte@screenly.io>
- * @license  https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPLv2
- * @link     https://github.com/Screenly/Screenly-Cast-for-WordPress
- * @since    0.0.1
+ * @package ScreenlyCast
  */
 defined('ABSPATH') or die("No script kiddies please!");
 
